f324a09420d21dcd081698f722eeadeecc59f377
dramosp <dramos@zytrust.com>
2023-09-29 13:48:29 -0500

    Agregando getter y setters

--- src/main/java/com/cibertec/FerreStockService/model/Tienda.java
+++ src/main/java/com/cibertec/FerreStockService/model/Tienda.java
@@ -29,5 +29,37 @@ public class Tienda {
 	
 	@Column(name = "DIRECCION_TIENDA", nullable = false)
 	private String direccion;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getRuc() {
+		return ruc;
+	}
+
+	public void setRuc(String ruc) {
+		this.ruc = ruc;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getDireccion() {
+		return direccion;
+	}
+
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
 	
 }
